0a774e795e4bc7f5f611c5bff456c74b5262b908
danielpiressm <danielpiressm@gmail.com>
2023-03-21 02:54:33 +0000

    adding video scene

---
+++ Assets/MetaAvatarsTemplate/Scripts/Mine/Countdown.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class Countdown : MonoBehaviour
+{
+    float timeToStartScene = 10.0f;
+    [SerializeField]
+    float timeRemaining = 10.0f;
+    bool timerIsRunning = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerIsRunning = true;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+
+                SceneManager.LoadScene("drawCube", LoadSceneMode.Single);
+                //
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+}
