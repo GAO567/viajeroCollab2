6e99f812548dc46677b39b7dd571079c6d759a1f
danielpiressm <danielpiressm@gmail.com>
2023-04-20 02:38:41 +0100

    Update Raycaster.cs

--- Assets/MetaAvatarsTemplate/Scripts/Mine/Raycaster.cs
+++ Assets/MetaAvatarsTemplate/Scripts/Mine/Raycaster.cs
@@ -161,7 +161,7 @@ public class Raycaster : MonoBehaviour
             
         }
 
-        lasttimeTriggered = triggered;
+        lasttimeTriggered = triggered || handTriggered;
     }
 
     private void OnTriggerEnter(Collider other)
