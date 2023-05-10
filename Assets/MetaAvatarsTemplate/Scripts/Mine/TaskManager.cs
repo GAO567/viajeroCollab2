3f4505e7bf07a3d2f2669b7da4d1bcf5d6b2fef5
danielpiressm <danielpiressm@gmail.com>
2023-05-10 17:24:05 +0100

    Update TaskManager.cs

--- Assets/MetaAvatarsTemplate/Scripts/Mine/TaskManager.cs
+++ Assets/MetaAvatarsTemplate/Scripts/Mine/TaskManager.cs
@@ -1391,6 +1391,7 @@ public class TaskManager : MonoBehaviour
         dominantplayer = "P1";
         taskStartedP2 = true;
         timeRemaining = totalTimePerTask;
+        isRemotePlayer = true;
     }
 
     void initializeTask()
