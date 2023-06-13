4bebe2a436fc0839c367e0e72aefca87874f992c
danielpiressm <danielpiressm@gmail.com>
2023-06-13 20:13:46 +0100

    fixing viewport

--- Assets/MetaAvatarsTemplate/Scripts/Mine/ViewportDrawer.cs
+++ Assets/MetaAvatarsTemplate/Scripts/Mine/ViewportDrawer.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class ViewportDrawer : MonoBehaviour
 {
     [SerializeField] Vector2 sizeViewport = new Vector2(0.3f,0.3f);
     [SerializeField] float distanceFromCamera = 1.0f;
-    public LineRenderer lineRenderer; 
-
+    public LineRenderer lineRenderer;
+    public Material remoteMaterial;
+    public Material localMaterial;
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +20,28 @@ public class ViewportDrawer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        lineRenderer.positionCount = 5;
 
+        GameObject rootObj = GameObject.Find("RootAreas");
+        if (rootObj)
+        {
+            PhotonView photonView = rootObj.GetComponent<PhotonView>();
+            if (photonView)
+            {
+
+                if (photonView.IsMine)
+                {
+                    lineRenderer.material = localMaterial;
+                }
+                else
+                {
+                    lineRenderer.material = remoteMaterial;
+                }
+
+            }
+        }
+
+
+        lineRenderer.positionCount = 5;
         Vector3 topLeft = new Vector3(-sizeViewport.x / 2.0f, sizeViewport.y / 2.0f, distanceFromCamera);
         Vector3 topRight = new Vector3(sizeViewport.x / 2.0f, sizeViewport.y / 2.0f, distanceFromCamera);
         Vector3 bottomRight = new Vector3(sizeViewport.x / 2.0f, -sizeViewport.y / 2.0f, distanceFromCamera);
